621f31628dec74f5669a13fddb7281ff2efe864f
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-07-28 15:29:34 +0430

    add products suggest

---
+++ src/Data/Data.ts
@@ -0,0 +1,196 @@
+ export interface IAmaZing{
+    id:number
+    name:string
+    size:string
+    type:string
+    price:number
+    discount:number
+    img:string
+    subImg:string
+}
+
+export interface IData{
+    amazing:IAmaZing[]
+}
+
+// export let Data:IData = {
+
+    export let  amazing:IAmaZing[] = [
+          {
+              id:1,
+              name:"Flirt",
+              size:"X",
+              type:"ساعت مچی",
+              price:49000,
+              discount:30,
+              img:"https://www.banimode.com/483588-large_default/42758.jpg",
+              subImg:"https://www.banimode.com/547932-large_default/47615.jpg"
+          },
+          {
+            id:2,
+            price:70000,
+            size:"XS",
+            discount:47,
+            type:"ساعت مچی",
+            name:"RNS",
+            img:"https://www.banimode.com/531441-large_default/46519.jpg",
+            subImg:"https://www.banimode.com/531439-large_default/46519.jpg"
+        },
+
+        {
+            id:3,
+            price:80000,
+            discount:27,
+            size:"Xl",
+            type:"ساعت مچی",
+            name:"Eloj",
+            img:"https://www.banimode.com/543778-large_default/47253.jpg",
+            subImg:"https://www.banimode.com/543777-large_default/47253.jpg"
+        },
+
+        {
+            id:4,
+            price:90000,
+            discount:57,
+            size:"S",
+            type:"ساعت مچی",
+            name:"Arnika",
+            img:"https://www.banimode.com/469082-large_default/41748.jpg",
+            subImg:"https://www.banimode.com/469081-large_default/41748.jpg"
+        },
+
+        {
+            id:5,
+            price:95000,
+            discount:49,
+            size:"M",
+            type:"ساعت مچی",
+            name:"Just",
+            img:"https://www.banimode.com/525653-large_default/45978.jpg",
+            subImg:"https://www.banimode.com/525652-large_default/45978.jpg"
+        },
+
+        {
+            id:6,
+            price:66000,
+            discount:39,
+            size:"XS",
+            type:"ساعت مچی",
+            name:"Avant",
+            img:"https://www.banimode.com/523423-large_default/45845.jpg",
+            subImg:"https://www.banimode.com/523422-large_default/45845.jpg"
+        },
+
+
+        
+        {
+            id:7,
+            price:26000,
+            discount:50,
+            size:"L",
+            type:"ساعت مچی",
+            name:"Brands",
+            img:"https://www.banimode.com/524668-large_default/45887.jpg",
+            subImg:"https://www.banimode.com/524669-large_default/45887.jpg"
+        },
+        {
+            id:8,
+            price:68000,
+            discount:52,
+            size:"M",
+            type:"ساعت مچی",
+            name:"Anly",
+            img:"https://www.banimode.com/536424-large_default/46808.jpg",
+            subImg:"https://www.banimode.com/536425-large_default/46808.jpg"
+        },
+      ]
+
+
+    // }
+
+
+        export let  suggests:IAmaZing[] = [
+          {
+              id:1,
+              name:"Flirt",
+              size:"X",
+              type:"ساعت مچی",
+              price:49000,
+              discount:30,
+              img:"https://www.banimode.com/551065-large_default/47826.jpg",
+              subImg:"https://www.banimode.com/547932-large_default/47615.jpg"
+          },
+          {
+            id:2,
+            price:70000,
+            size:"XS",
+            discount:47,
+            type:"ساعت مچی",
+            name:"RNS",
+            img:"https://www.banimode.com/548876-large_default/47684.jpg",
+            subImg:"https://www.banimode.com/548875-large_default/47684.jpg"
+        },
+
+        {
+            id:3,
+            price:80000,
+            discount:27,
+            size:"Xl",
+            type:"ساعت مچی",
+            name:"Eloj",
+            img:"https://www.banimode.com/548086-large_default/47546.jpg",
+            subImg:"https://www.banimode.com/548085-large_default/47546.jpg"
+        },
+
+        {
+            id:4,
+            price:90000,
+            discount:57,
+            size:"S",
+            type:"ساعت مچی",
+            name:"Arnika",
+            img:"https://www.banimode.com/551038-large_default/47887.jpg",
+            subImg:"https://www.banimode.com/551037-large_default/47887.jpg"
+        },
+
+        {
+            id:5,
+            price:95000,
+            discount:49,
+            size:"M",
+            type:"ساعت مچی",
+            name:"Just",
+            img:"https://www.banimode.com/550840-large_default/47838.jpg",
+            subImg:"https://www.banimode.com/550839-large_default/47838.jpg"
+        },
+
+        {
+            id:6,
+            price:66000,
+            discount:39,
+            size:"XS",
+            type:"ساعت مچی",
+            name:"Avant",
+            img:"https://www.banimode.com/518722-large_default/45264.jpg",
+            subImg:"https://www.banimode.com/522084-large_default/45264.jpg"
+        },
+
+
+        
+        {
+            id:7,
+            price:26000,
+            discount:50,
+            size:"L",
+            type:"ساعت مچی",
+            name:"Brands",
+            img:"https://www.banimode.com/550867-large_default/47822.jpg",
+            subImg:"https://www.banimode.com/550866-large_default/47822.jpg"
+        },
+      
+      ]
+
+
+// export const productsNew = [
+
+// ]
---
+++ src/components/ProductItemsSuggest/ProductItemSuggest.tsx
@@ -0,0 +1,99 @@
+import React ,{useEffect}from 'react'
+import { BiHeart} from "react-icons/bi";
+import { suggests,IAmaZing } from '../../Data/Data';
+
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
+import "swiper/components/navigation/navigation.min.css"
+
+import "./ProductItemSuggest.style.scss"
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Pagination,Navigation,Autoplay
+} from 'swiper/core';
+
+// install Swiper modules
+SwiperCore.use([Autoplay,Pagination,Navigation]);
+
+const ProductItemSuggest = () => {
+
+        const [productItems,setProductItems] = React.useState<IAmaZing[]>(suggests)
+  
+        const handleChangeImg =(id:number):void=>{
+            setProductItems(productItems.map((item:IAmaZing)=>item.id === id ?{...item,img:item.subImg} :item))
+        }
+          
+         const handleChangeLiveImg =(id:number):void=>{
+         setProductItems(suggests)
+     }
+    return (
+        <Swiper 
+        autoplay={{"delay": 3000, "disableOnInteraction": false}}
+        //  slidesPerView={5} 
+          spaceBetween={15} 
+          slidesPerGroup={1} 
+          navigation={true}
+          loop={true}
+           loopFillGroupWithBlank={true}
+           breakpoints={{
+            "340": {
+              "slidesPerView": 1,
+              "spaceBetween": 20,
+
+            },
+            "640": {
+              "slidesPerView": 2,
+            
+            },
+            "768": {
+              "slidesPerView": 4,
+           
+            },
+            "1024": {
+              "slidesPerView": 4,
+              
+            }
+          }}
+     
+            pagination={false}  className="mySwiper">
+          {
+            productItems.map((item:IAmaZing,index:number)=>(
+              <SwiperSlide key={item.id}>
+                 <div className="d-flex flex-column" onMouseLeave={()=>handleChangeLiveImg(item.id)} onMouseEnter={()=> handleChangeImg(item.id)}>
+                      <div className="product___card">
+                      <img src={item.img} alt={item.name} className={item.name} />
+                      <small className="descount">%{item.discount}</small>
+                      <div className="product___heart">
+                         <BiHeart  size={28} className="d-lg-none"/>
+                      </div>
+                    </div>
+                    <div className="d-flex justify-content-between py-3">
+                      <strong>{item.name}</strong>
+                      <small className="text-decoration-line-through text-muted"> {item.price}{" "}تومان</small>
+                    </div>
+
+                    <div className="d-flex justify-content-between pt-3 pb-1">
+                      <p className="m-0">{item.type}</p>
+                      <small className="product___card-price"> { item.price - (item.price * item.discount) / 100}{" "}تومان</small>
+                    </div>
+                    <div className="py-3 d-flex justify-content-start">
+                       <small className="product__card-size mb-2">{item.size}</small>
+                    </div>
+                 </div>
+              </SwiperSlide>
+            ))
+          }
+
+        
+      </Swiper>
+    )
+}
+
+export default ProductItemSuggest
