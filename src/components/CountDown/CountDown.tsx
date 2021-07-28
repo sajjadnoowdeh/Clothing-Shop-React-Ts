b026c6b1c3360f38a383ab9b99ba7984540849a4
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-07-28 15:31:16 +0430

    add count down

---
+++ src/components/CountDown/CountDown.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useTimer } from 'react-timer-hook';
+import "./CountDown.style.scss";
+function MyTimer({ expiryTimestamp }:any) {
+    const {
+      seconds,
+      minutes,
+      hours,
+      days,
+      isRunning,
+      start,
+      pause,
+      resume,
+      restart,
+    } = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called') });
+    return (
+        <div style={{textAlign: 'center'}}>
+      
+          <div className="section__timer">
+            <span className="px-3 timer" >{days}</span>:<span className="px-3 timer" >{hours}</span>:<span className="px-3 timer" >{minutes}</span>:<span className="px-3 timer">{seconds}</span>
+          </div>
+          {/* <p>{isRunning ? 'Running' : 'Not running'}</p> */}
+          {/* <button onClick={start}>Start</button>
+          <button onClick={pause}>Pause</button>
+          <button onClick={resume}>Resume</button> */}
+          {/* <button onClick={() => {
+            // Restarts to 5 minutes timer
+            const time = new Date();
+            time.setSeconds(time.getSeconds() + 300);
+            restart(time)
+          }}>Restart</button> */}
+        </div>
+      ); 
+    }
+
+interface ICountDown{
+    timeCustom:number
+}
+
+const CountDown:React.FC<ICountDown> = ({timeCustom}) => {
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + timeCustom); // 10 minutes timer
+    return (
+        <MyTimer expiryTimestamp={time} />
+    )
+}
+
+export default CountDown
