1f9b22e388cf1b63c745fb812b78547d507402d3
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-08-03 10:34:17 +0430

    add SidebarFilterSize

---
+++ src/components/Sidebar/SidebarFilterSize/SidebarFilterSize.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./SidebarFilterSize.style.scss"
+const SidebarFilterSize = () => {
+    return (
+        <div className="sidebar__sizing py-4">
+        <p > انداره</p>
+
+    <select name="filter-size">
+        <option value="">All</option>
+         <option value="X"  >X</option>
+         <option  value="XL" >XL</option>
+         <option  value="M" >M</option>
+         <option  value="L" >L</option>
+         <option  value="XS" >XS</option>
+         <option value="S"  >S</option>
+      </select>
+
+      </div>
+    )
+}
+
+export default SidebarFilterSize
