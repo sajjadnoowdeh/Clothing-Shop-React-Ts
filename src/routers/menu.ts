c1cdcd05b07d558866fbafc0492f285e82d1f47f
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-08-03 10:35:56 +0430

    add links to menu

---
+++ src/routers/menu.ts
@@ -0,0 +1,25 @@
+export const links =[
+      {link:"/newset/famale",title:"زنانه"},
+      {link:"/newset/male",title:"مردانه"},
+      {link:"/newset/baby",title:"بچگانه"}
+]
+
+
+export const linksMale =[
+    {link:"/male/Tshirt",title:"تیشرت"},
+    {link:"/male/shirt",title:"بیراهن"},
+    {link:"/male/cout",title:"کت و شلوار"},
+
+]
+export const linksFamale=[
+    {link:"/famale/munto",title:"مانتو"},
+    {link:"/famale/pants",title:"شلوار"},
+    {link:"/famale/skirt",title:"دامن"},
+
+]
+
+export const linksBaby=[
+    {link:"/baby/babyDress",title:"لباس نوزاد"},
+    {link:"/baby/jacket",title:" کاپشن"},
+
+]
