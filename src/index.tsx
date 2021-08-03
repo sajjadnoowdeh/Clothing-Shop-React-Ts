24b1fec3f17bb055d1fbd97e41ce6cccdd67f267
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-08-03 10:23:41 +0430

    add Suspense to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Spinner } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 import App from './App';
@@ -7,7 +8,7 @@ import { Suspense } from 'react';
 
 
 ReactDOM.render(
- <Suspense fallback={ <div>Loadin...</div> }>
+ <Suspense fallback={<div className="spinner"><Spinner style={{fontSize:"34px"}} animation="border" variant="success" /> </div> }>
  
    <App />
  </Suspense>,
