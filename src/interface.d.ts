4e76173c2efc32e15f867e080ad0cce937392648
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-08-04 12:41:06 +0430

    update interface

--- src/interface.d.ts
+++ src/interface.d.ts
@@ -1,14 +1,13 @@
+
+
 export interface IProduct{
     id:number
     name:string
     size:string
     type:string
+    category:string
     price:number
     discount?:number
     img:string
     subImg:string
-}
-
-export interface IData{
-    amazing:IProduct[]
 }
