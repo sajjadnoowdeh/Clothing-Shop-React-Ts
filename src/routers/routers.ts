5dc0bc1732365232453dcb0968b60ae56ff093ab
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-08-09 18:41:33 +0430

    add route not found

--- src/routers/routers.ts
+++ src/routers/routers.ts
@@ -2,11 +2,15 @@ import React from "react";
 const HomePage = React.lazy(()=> import("../pages/HomePage/HomePage"));
 const ProductSinglePage= React.lazy(()=> import("../pages/ProductSinglePage/ProductSinglePage"))
 const CategoryProducts = React.lazy(()=>import("../pages/CategoryProducts/CategoryProducts"))
+const CategoryProductsMobile = React.lazy(()=>import("../pages/CategoryProductsMobile/CategoryProductMobile"))
+const NotFoundPage = React.lazy(()=>import("../pages/NotFoundPage/NotFoundPage"))
+
 export let routers = [
         {path:"/", exact:true, Component:HomePage},
+         {path:"/categoryMobile", exact:true, Component:CategoryProductsMobile},
         {path:"/category/:category_name/", exact:true, Component:CategoryProducts},
         {path:"/category/:category_name/:id", exact:true, Component:ProductSinglePage},
-
+        {path:"*", exact:true, Component:NotFoundPage},
 
 
 ]
