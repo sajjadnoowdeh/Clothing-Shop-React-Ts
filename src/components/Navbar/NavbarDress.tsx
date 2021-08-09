86b114cc13d9ba0864fbfb014cf853f27bcd4a0a
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-08-09 18:35:22 +0430

    add btn heart

--- src/components/Navbar/NavbarDress.tsx
+++ src/components/Navbar/NavbarDress.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Container,Navbar,Badge,Nav,NavDropdown,Form,FormControl,Button} from "react-bootstrap"
 import { BiShoppingBag,BiSearch } from "react-icons/bi";
+import { BiHeart} from "react-icons/bi";
 import { Link } from 'react-router-dom';
 import "./NavbarDress.style.scss";
 export default function NavbarDress() {
@@ -8,12 +9,14 @@ export default function NavbarDress() {
       <Container className="navbar-dress py-4 d-flex align-items-center justify-content-between">
         <div className="order-lg-1 brand " ><Link to="/" className="d-flex justify-content-center justify-content-lg-end"><img className="img-brand" src="https://www.banimode.com//themes/new/assets/images/banilogo.png" alt="" />  </Link></div>
   
-         <div className="d-none d-lg-flex ">
+         <div className="d-none d-lg-flex  align-items-center">
+          
          <Button className="basket-btn" style={{backgroundColor:"none"}}>
             <span className="visually-hidden">unread messages</span>
              <Badge className="count-shop">9</Badge>
             <BiShoppingBag color="black" size={35}/>
           </Button>
+          <BiHeart size={35} className="ms-3"/>
           <div className="login d-flex align-items-center ms-2">
               <a className="text-decoration-none"  href="#"> ورود</a>
               <span className="px-1">/</span>
