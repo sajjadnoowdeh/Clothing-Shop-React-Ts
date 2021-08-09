e7626fe9bad6d757ce65c39ef6ad17d5791498fc
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-08-09 18:33:22 +0430

    add Provider redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Spinner } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.scss';
-import App from './App';
-import { Suspense } from 'react';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Spinner } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.scss";
+import App from "./App";
+import store from "./Store/reducers/store";
+import { Provider } from "react-redux";
+import { Suspense } from "react";
 
 ReactDOM.render(
- <Suspense fallback={<div className="spinner"><Spinner style={{fontSize:"34px"}} animation="border" variant="success" /> </div> }>
- 
-   <App />
- </Suspense>,
-  
-  document.getElementById('root')
-);
+  <Provider store={store}>
+    <Suspense
+      fallback={
+        <div className="spinner">
+          <Spinner
+            style={{ fontSize: "34px" }}
+            animation="border"
+            variant="success"
+          />{" "}
+        </div>
+      }
+    >
+      <App />
+    </Suspense>
+  </Provider>,
 
+  document.getElementById("root")
+);
