c150cdb769600e188a59527583561dd1cb544ea0
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-08-11 08:28:26 +0430

    add actions reducers

--- src/Store/reducers/product.reducer/product.reducer.ts
+++ src/Store/reducers/product.reducer/product.reducer.ts
@@ -2,9 +2,11 @@ import {createSlice,PayloadAction} from "@reduxjs/toolkit"
 import {products} from "../../../Data/Data";
 import {IProduct} from "../../../interface"
 interface IState{
+    cart:IProduct[]
     productsData:IProduct[]
 }
 let init:IState = {
+    cart:[],
     productsData: products
 }
  const productReducer = createSlice({
@@ -20,8 +22,28 @@ let init:IState = {
      },
      productChangeSubImg:(state,action: PayloadAction<string>)=>{
         state.productsData = init.productsData.filter((item)=>item.category ===action.payload)
-     }
+     },
+    //  add To Cart
+    addToCart:(state,action)=>{
+        state.cart.push(action.payload)
+    },
+    updateCart:(state,action: PayloadAction<number>)=>{
+        state.cart = state.cart.map((item)=>item.id === action.payload ? {...item,count:item.count + 1} : item)
+    },
+    updateCountCard:(state,action: PayloadAction<number>)=>{
+        state.cart = state.cart.map((item)=>item.id === action.payload ? {...item,count: item.count + 1} :item)
+    },
+    updateCountMinusCard:(state,action: PayloadAction<number>)=>{
+        state.cart = state.cart.map((item)=>item.id === action.payload ? {...item,count: item.count - 1} :item)
+    },
+    removeProductCard:(state,action: PayloadAction<number>)=>{
+        state.cart = state.cart.filter((item)=>item.id !== action.payload)
+    },
+    checkCountCart:(state,action: PayloadAction<number>)=>{
+       state.cart =  state.cart.map((item)=>item.id === action.payload ? {...item,count: 1} :item)
+    }
+
  }
 })
-export const {productsCategoryFilter,productChangeImg,productChangeSubImg} = productReducer.actions;
+export const {productsCategoryFilter,productChangeImg,productChangeSubImg,addToCart,updateCart,updateCountCard,updateCountMinusCard,checkCountCart,removeProductCard} = productReducer.actions;
 export default productReducer.reducer;
