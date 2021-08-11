232944958beb09d40ac74d3f4d1de1edeef09526
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-08-11 08:28:10 +0430

    add router cart

--- src/routers/routers.ts
+++ src/routers/routers.ts
@@ -4,10 +4,11 @@ const ProductSinglePage= React.lazy(()=> import("../pages/ProductSinglePage/Prod
 const CategoryProducts = React.lazy(()=>import("../pages/CategoryProducts/CategoryProducts"))
 const CategoryProductsMobile = React.lazy(()=>import("../pages/CategoryProductsMobile/CategoryProductMobile"))
 const NotFoundPage = React.lazy(()=>import("../pages/NotFoundPage/NotFoundPage"))
-
+const CartPage = React.lazy(()=>import("../pages/CartPage/CartPage"))
 export let routers = [
         {path:"/", exact:true, Component:HomePage},
-         {path:"/categoryMobile", exact:true, Component:CategoryProductsMobile},
+        {path:"/cart", exact:true, Component:CartPage},
+        {path:"/categoryMobile", exact:true, Component:CategoryProductsMobile},
         {path:"/category/:category_name/", exact:true, Component:CategoryProducts},
         {path:"/category/:category_name/:id", exact:true, Component:ProductSinglePage},
         {path:"*", exact:true, Component:NotFoundPage},
