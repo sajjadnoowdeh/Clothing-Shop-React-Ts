68fd6f648a79c999836c46c965ed3eba931b8d57
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-08-18 22:49:06 +0430

    add action sort

--- frontend/src/components/ProductSorted/ProductSorted.tsx
+++ frontend/src/components/ProductSorted/ProductSorted.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import "./ProductSorted.style.scss"
-const ProductStored = () => {
+interface IProductSorted{
+  handleSortLowest:Function
+  handleHighestPrice:Function
+  handleHighestDiscountPrice:Function
+  handleDefaultSort:Function
+}
+const ProductStored:React.FC<IProductSorted> = ({handleSortLowest,handleHighestPrice,handleHighestDiscountPrice,handleDefaultSort}) => {
     return (
         <div className="sidebar-sorted mb-4 d-flex align-items-center">
         <span className="me-3">مرتب سازی: </span>
         <ul className="d-flex align-items-center justify-content-evenly m-0">
-           <li className="active-sorted{">پیش فرض</li>
-           <li >بیشترین تخفیف</li>
-           <li>بیشترین قیمت</li>
-           <li>کمترین قیمت</li>
+           <li onClick={()=>handleDefaultSort()} className="active-sorted">پیش فرض</li>
+           <li onClick={()=>handleHighestDiscountPrice()} className="active-sorted">بیشترین تخفیف</li>
+           <li onClick={()=>handleHighestPrice()} className="active-sorted">بیشترین قیمت</li>
+           <li onClick={()=>handleSortLowest()} className="active-sorted">کمترین قیمت</li>
         </ul>
       </div>
     )
